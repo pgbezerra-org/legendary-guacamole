fd2662a9fb99d45adf5e4fb3f2caa00fce6c531b
Matheus Lacerda <matheus.l1996@gmail.com>
2023-04-28 15:23:59 -0300

    commented code at appsettings.json and program.cs

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
 
@@ -10,7 +11,12 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
-
+/*
+//string mysqlcon = builder.Configuration.GetConnectionString("mysqlcon");
+string mysqlcon = "server=(localdb)\\MSSQLLocalDB;user=root;database=RazorPagesMovieContext-bd990c08-ec81-438a-8d77-80da93634a86;port=3306;password=livro";
+MySqlConnection con = new MySqlConnection(mysqlcon);
+con.Open();
+*/
 var app = builder.Build();
 /*
 //Creates some Movies ("seed data.cs")
