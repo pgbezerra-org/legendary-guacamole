5517b43fe8e030cea55987e303d3875f72b12198
Matheus Lacerda <matheus.l1996@gmail.com>
2023-05-24 23:42:24 -0300

    login done

--- webserver/Pages/Account/Register.cshtml.cs
+++ webserver/Pages/Account/Register.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using webserver.Data;
 using webserver.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace webserver.Pages.Account {
 
