82cfce93a71f55b108711d8394204bce1af109e9
Matheus Lacerda <matheus.l1996@gmail.com>
2023-05-29 19:11:38 -0300

    webserverContext now inheriting from identitydbcontext

--- webserver/Data/webserverContext.cs
+++ webserver/Data/webserverContext.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using webserver.Models;
 
 namespace webserver.Data
 {
-    public class webserverContext : DbContext
-    {
+    public class webserverContext : IdentityDbContext<Company, IdentityRole, string> {
+
         public webserverContext (DbContextOptions<webserverContext> options)
             : base(options)
         {
