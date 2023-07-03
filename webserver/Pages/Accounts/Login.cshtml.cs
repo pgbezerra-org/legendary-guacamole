84f135845b2881ea41d1db0eb29ec92d57888714
Matheus Lacerda <matheus.l1996@gmail.com>
2023-07-03 18:53:53 -0300

    Worker Login sketched

--- webserver/Pages/Accounts/Login.cshtml.cs
+++ webserver/Pages/Accounts/Login.cshtml.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using webserver.Models;
 
 namespace webserver.Pages.Account {
 
@@ -10,48 +12,83 @@ namespace webserver.Pages.Account {
 
         [BindProperty]
         public Credential Credential { get; set; } = new Credential();
+        
+        public readonly static string loginCookie = "BZEmploy";
+
+        private readonly SignInManager<Company> _signInManager;
+        private readonly UserManager<Company> _userManager;
+
+        public LoginModel(SignInManager<Company> signInManager, UserManager<Company> userManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
         public void OnGet() {
-            
+
         }
 
         public async Task<IActionResult> OnPostAsync() {
 
-            if (!ModelState.IsValid) {
+            /*if (!ModelState.IsValid) {
+                return Page();
+            }*/
+
+            var user = await _userManager.FindByEmailAsync(Credential.Email);
+
+            if (user == null) {
+                ModelState.AddModelError(string.Empty, "User not registered!");
                 return Page();
             }
 
-            if (Credential.UserName == "admin" && Credential.Password == "password") {
-
-                var claims = new List<Claim> {
-                    new Claim(ClaimTypes.Name, "admin"),
-                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
-                };
-
-                var claimsIdentity = new ClaimsIdentity(claims, "MyCookieAuth");
-                var authProperties = new AuthenticationProperties {
-                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    IsPersistent = false,
-                    IssuedUtc = DateTimeOffset.UtcNow
-                };
-
-                await HttpContext.SignInAsync("MyCookieAuth", new ClaimsPrincipal(claimsIdentity), authProperties);
-
-                return RedirectToPage("/Index");
+            //Just to stop with the compiler warnings
+            // Ensure the user properties are not null before accessing them
+            if (user.PasswordHash is null || user.UserName is null || user.Email is null) {
+                // Handle the case where one or more properties are null
+                return Page();
             }
 
-            return Page();
+            var passwordHasher = new PasswordHasher<Company>();
+            var passwordResult = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, Credential.Password);
+
+            if (passwordResult != PasswordVerificationResult.Success) {
+                ModelState.AddModelError(string.Empty, "Wrong Email or Password");
+                return Page();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, Credential.Password, isPersistent: true, lockoutOnFailure: false);
+
+            if (!result.Succeeded) {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. " + result.ToString());
+                return Page();
+            }
+
+            var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Email, user.Email)
+                };
+
+            var claimsIdentity = new ClaimsIdentity(claims, "MyCookieAuth");
+            var authProperties = new AuthenticationProperties {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = true,
+                IssuedUtc = DateTimeOffset.UtcNow
+            };
+
+            await HttpContext.SignInAsync("MyCookieAuth", new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            // Redirect to the desired page after successful login
+            return RedirectToPage("/Privacy");
         }
-    }
 
-    public class Credential {
+        public class Credential {
 
-        [System.ComponentModel.DataAnnotations.Required]
-        public string UserName { get; set; } = string.Empty;
+            [System.ComponentModel.DataAnnotations.Required]
+            public string UserName { get; set; } = string.Empty;
 
-        [System.ComponentModel.DataAnnotations.Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
+            [System.ComponentModel.DataAnnotations.Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = string.Empty;
 
+        }
     }
 }
