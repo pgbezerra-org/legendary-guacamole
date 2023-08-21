e80ed1d5ed440ac8cb172c9047837ea57afdbb7d
Matheus Lacerda <math2.0@hotmail.com>
2023-08-21 02:01:21 -0300

    Employees List page

---
+++ webserver/Pages/Manage/EmployeesLIST.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using MySqlConnector;
+
+namespace webserver.Pages.Manage 
+{
+    [Authorize]    
+    public class EmployeesLIST : PageModel {
+        public List<EmployeeINFO> listEmpls=new List<EmployeeINFO>();
+        
+        public EmployeesLIST() {
+            
+        }
+        
+        public void OnGet() {
+            string MyConnection= "server=localhost;port=3306;database=Guacamole;user=root;password=xpvista7810";
+
+            using (MySqlConnection connection=new MySqlConnection(MyConnection)){
+                connection.Open();
+
+                string allComps = "SELECT b.*, u.UserName FROM BZEmployees b JOIN AspNetUsers u ON b.Id = u.Id";
+                using (MySqlCommand command = new MySqlCommand(allComps, connection)){
+                    using (MySqlDataReader reader=command.ExecuteReader()){
+                        while(reader.Read()){
+                            EmployeeINFO bzeINFO = new EmployeeINFO();
+                            bzeINFO.Id=reader.GetString(0);
+                            bzeINFO.UserName=reader.GetString(1);
+                            listEmpls.Add(bzeINFO);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class EmployeeINFO{
+    public string Id { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+}
