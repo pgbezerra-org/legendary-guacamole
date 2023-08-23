810a30a1f76db98982ff8ae07c76d6a0de256d02
Matheus Lacerda <math2.0@hotmail.com>
2023-08-22 23:42:09 -0300

    RECRUD listado

--- webserver/Pages/Account/LogOut.cshtml.cs
+++ webserver/Pages/Account/LogOut.cshtml.cs
@@ -3,32 +3,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-namespace webserver.Pages.Accounts
-{
-    public class LogOut : PageModel
-    {
+namespace webserver.Pages.Accounts {
+    public class LogOut : PageModel {
+        
         private readonly ILogger<LogOut> _logger;
 
-        public LogOut(ILogger<LogOut> logger)
-        {
+        public LogOut(ILogger<LogOut> logger) {
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public void OnGet() {
         }
 
-        public async Task<IActionResult> OnPostAsync(string logout)
-    {
-        if (logout == "Yes")
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToPage("/Accounts/Login");
+        public async Task<IActionResult> OnPostAsync(string logout) {
+            if (logout == "Yes") {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return RedirectToPage("/Accounts/Login");
+            }else {
+                return RedirectToPage("/Index");
+            }
         }
-        else
-        {
-            return RedirectToPage("/Index");
-        }
-    }
+
     }
 }
--- webserver/Pages/Manage/RECRUD.cshtml.cs
+++ webserver/Pages/Manage/RECRUD.cshtml.cs
@@ -1,27 +1,39 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySqlConnector;
-using webserver.Data;
 
-namespace webserver.Pages.Manage
-{
+namespace webserver.Pages.Manage {
     [Authorize]
     
-    public class RECRUD : PageModel
-    {
+    public class RECRUD : PageModel {
+
         public List<RealEstateINFO> listREINFOs=new List<RealEstateINFO>();
+
+        public int rowCount, index, size;
         
         public RECRUD() {
             
         }
 
-        public void OnGet()
-        {
+        public void OnGet(int pageIndex =1, int pageSize=5) {
             string MyConnection= "server=localhost;port=3306;database=Guacamole;user=root;password=xpvista7810";
 
             using (MySqlConnection connection=new MySqlConnection(MyConnection)){
                 connection.Open();
-                string allREs = "SELECT RealEstates.*, AspNetUsers.* FROM RealEstates INNER JOIN AspNetUsers ON RealEstates.CompanyId = AspNetUsers.Id"; //limit search at a later date...
+
+                string countQuery = "SELECT COUNT(*) FROM RealEstates";
+                using (MySqlCommand command = new MySqlCommand(countQuery, connection)){
+                    rowCount = Convert.ToInt32(command.ExecuteScalar());
+                    index=pageIndex;
+                    size=pageSize;
+                }
+            }
+            
+            using (MySqlConnection connection=new MySqlConnection(MyConnection)){
+                connection.Open();
+                int offset=(pageIndex-1)*pageSize;
+                string allREs = $"SELECT RealEstates.*, AspNetUsers.* FROM RealEstates INNER JOIN AspNetUsers ON RealEstates.CompanyId = AspNetUsers.Id LIMIT {pageSize} OFFSET {offset}"; //limit search at a later date...
                 using (MySqlCommand command = new MySqlCommand(allREs, connection)){
                     using (MySqlDataReader reader=command.ExecuteReader()){
                         while(reader.Read()){
