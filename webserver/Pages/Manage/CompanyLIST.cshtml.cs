0cd6677a6c3bc589a440b4ecdc574f379e236662
Matheus Lacerda <math2.0@hotmail.com>
2023-08-22 23:42:09 -0300

    RECRUD listado

--- webserver/Pages/Manage/CompanyLIST.cshtml.cs
+++ webserver/Pages/Manage/CompanyLIST.cshtml.cs
@@ -19,13 +19,13 @@ namespace webserver.Pages.Manage
             
         }
         
-        public void OnGet() {
+        public void OnGet(int page =1, int pageSize=5) {
             string MyConnection= "server=localhost;port=3306;database=Guacamole;user=root;password=xpvista7810";
 
             using (MySqlConnection connection=new MySqlConnection(MyConnection)){
                 connection.Open();
-
-                string allComps = "SELECT c.*, u.UserName FROM Company c JOIN AspNetUsers u ON c.Id = u.Id";
+                int offset=(page-1)*pageSize;
+                string allComps = $"SELECT c.*, u.UserName FROM Company c JOIN AspNetUsers u ON c.Id = u.Id LIMIT {pageSize} OFFSET {offset}";
                 using (MySqlCommand command = new MySqlCommand(allComps, connection)){
                     using (MySqlDataReader reader=command.ExecuteReader()){
                         while(reader.Read()){
