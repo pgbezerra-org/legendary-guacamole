08d7e65233786e1e295641ae50138168a8097622
Matheus Lacerda <math2.0@hotmail.com>
2023-08-23 00:13:32 -0300

    employeeList paginado

--- webserver/Pages/Manage/EmployeesLIST.cshtml.cs
+++ webserver/Pages/Manage/EmployeesLIST.cshtml.cs
@@ -8,18 +8,30 @@ namespace webserver.Pages.Manage
     [Authorize(Roles=Common.BZE_Role)]
     public class EmployeesLIST : PageModel {
         public List<EmployeeINFO> listEmpls=new List<EmployeeINFO>();
+
+        public int rowCount, index, size;
         
         public EmployeesLIST() {
             
         }
         
-        public void OnGet() {
+        public void OnGet(int pageIndex =1, int pageSize=5) {
             string MyConnection= "server=localhost;port=3306;database=Guacamole;user=root;password=xpvista7810";
+
+            using (MySqlConnection connection = new MySqlConnection(MyConnection)) {
+                connection.Open();
+                string countQuery = "SELECT COUNT(*) FROM (SELECT b.*, u.UserName FROM BZEmployees b JOIN AspNetUsers u ON b.Id = u.Id) AS subquery";
+                using (MySqlCommand command = new MySqlCommand(countQuery, connection)) {
+                    rowCount = Convert.ToInt32(command.ExecuteScalar());
+                    index=pageIndex;
+                    size=pageSize;
+                }
+            }
 
             using (MySqlConnection connection=new MySqlConnection(MyConnection)){
                 connection.Open();
-
-                string allComps = "SELECT b.*, u.UserName FROM BZEmployees b JOIN AspNetUsers u ON b.Id = u.Id";
+                int offset=(pageIndex-1)*pageSize;
+                string allComps = $"SELECT b.*, u.UserName FROM BZEmployees b JOIN AspNetUsers u ON b.Id = u.Id LIMIT {pageSize} OFFSET {offset}"; //limit search at a later date...
                 using (MySqlCommand command = new MySqlCommand(allComps, connection)){
                     using (MySqlDataReader reader=command.ExecuteReader()){
                         while(reader.Read()){
