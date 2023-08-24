86eb3d7124418520399e91632e88d13ec0b54353
Matheus Lacerda <math2.0@hotmail.com>
2023-08-24 02:47:50 -0300

    Phone added to Company & Client creations

--- webserver/Pages/Manage/ClientCreation.cshtml.cs
+++ webserver/Pages/Manage/ClientCreation.cshtml.cs
@@ -12,44 +12,17 @@ namespace webserver.Pages.Manage {
     public class ClientCreation : PageModel {
 
         [BindProperty]
-        public InputModel Input { get; set; } = new InputModel();
+        public ClientINFO Input { get; set; } = new ClientINFO();
 
         private readonly UserManager<Client> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public ClientCreation(UserManager<Client> userManager, RoleManager<IdentityRole> roleManager)
-        {
+        public ClientCreation(UserManager<Client> userManager, RoleManager<IdentityRole> roleManager) {
             _userManager = userManager;
             _roleManager = roleManager;
         }
 
-        public class InputModel
-        {
-            [Required]
-            [Display(Name = "Name")]
-            public string Name { get; set; }=string.Empty;
-
-            [Required]
-            [Display(Name = "Email")]
-            public string Email { get; set; }=string.Empty;
-
-            [Required]
-            [Display(Name = "Occupation")]
-            public string Occupation { get; set; }=string.Empty;
-
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }=string.Empty;
-
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "ConfirmPassword")]
-            public string ConfirmPassword { get; set; }=string.Empty;
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
+        public async Task<IActionResult> OnPostAsync() {
             /*
             if (ModelState.IsValid){
                 return;
@@ -67,7 +40,7 @@ namespace webserver.Pages.Manage {
                 return Page();
             }
 
-            var client = new Client { UserName = Input.Name, Email = Input.Email, Occupation = Input.Occupation };
+            var client = new Client { UserName = Input.Name, Email = Input.Email, PhoneNumber = Input.PhoneNumber,  Occupation = Input.Occupation };
 
             var result = await _userManager.CreateAsync(client, Input.Password);
 
@@ -89,6 +62,36 @@ namespace webserver.Pages.Manage {
 
                 return Page();
             }
+        }
+
+        public class ClientINFO {
+            [Required]
+            [Display(Name = "Name")]
+            public string Name { get; set; }=string.Empty;
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }=string.Empty;
+
+            [Required]
+            [Display(Name = "PhoneNumber")]
+            [Phone]
+            public string PhoneNumber { get; set; }=string.Empty;
+
+            [Required]
+            [Display(Name = "Occupation")]
+            public string Occupation { get; set; }=string.Empty;
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }=string.Empty;
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "ConfirmPassword")]
+            public string ConfirmPassword { get; set; }=string.Empty;
         }
     }
 }
