ba234017853b5eef93cf1bf7f750c2ddd0eba750
Matheus Lacerda <math2.0@hotmail.com>
2023-08-24 02:39:18 -0300

    improved main page

--- webserver/Pages/Index.cshtml.cs
+++ webserver/Pages/Index.cshtml.cs
@@ -1,15 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using webserver.Data;
 
 namespace webserver.Pages {
     public class IndexModel : PageModel {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger) {
-            _logger = logger;
+        private readonly WebserverContext context;
+
+        [BindProperty]
+        public RealEstateINFO[] states {get; set;}=new RealEstateINFO[3];
+
+        public IndexModel(WebserverContext _context) {
+            context = _context;
         }
 
         public void OnGet() {
+            
+            var query = context.RealEstates
+                .OrderByDescending(r => r.Id)
+                .Take(3)
+                .Select(r => new RealEstateINFO { Name = r.Name, Address = r.Address, Price = r.Price });
 
+            states = query.ToArray();
+
+        }
+
+        public class RealEstateINFO{
+            public string Name { get; set;}=string.Empty;
+            public string Address { get; set;}=string.Empty;
+
+            public decimal Price;
         }
     }
 }
