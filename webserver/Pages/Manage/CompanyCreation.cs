b3767da5bfada8732b5ee629012899ca3df022a0
Matheus Lacerda <math2.0@hotmail.com>
2023-08-24 02:47:50 -0300

    Phone added to Company & Client creations

--- webserver/Pages/Manage/CompanyCreation.cs
+++ webserver/Pages/Manage/CompanyCreation.cs
@@ -12,26 +12,70 @@ namespace webserver.Pages.Manage {
     public class CompanyCreation : PageModel {
 
         [BindProperty]
-        public InputModel Input { get; set; } = new InputModel();
+        public CompanyINFO Input { get; set; } = new CompanyINFO();
 
         private readonly UserManager<Company> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public CompanyCreation(UserManager<Company> userManager, RoleManager<IdentityRole> roleManager)
-        {
+        public CompanyCreation(UserManager<Company> userManager, RoleManager<IdentityRole> roleManager) {
             _userManager = userManager;
             _roleManager = roleManager;
         }
 
-        public class InputModel
-        {
+        public async Task<IActionResult> OnPostAsync() {
+            /*
+            if (ModelState.IsValid){
+                return;
+            }
+            */
+            var auxUser = await _userManager.FindByEmailAsync(Input.Email);
+            if (_userManager == null) {
+                ModelState.AddModelError(string.Empty, "manager not registered!");
+                return Page();
+            }
+            if(auxUser != null){
+                ModelState.AddModelError(string.Empty, "Email already registered!");
+                return Page();
+            }
+
+            var company = new Company { UserName = Input.Name, Email = Input.Email, PhoneNumber = Input.PhoneNumber, Country = Input.Country, State = Input.State, City = Input.City };
+
+            var result = await _userManager.CreateAsync(company, Input.Password);
+
+            if (result.Succeeded) {
+
+                var roleExist = await _roleManager.RoleExistsAsync(Common.Company_Role);
+                if (!roleExist){
+                    await _roleManager.CreateAsync(new IdentityRole(Common.Company_Role));
+                }
+
+                await _userManager.AddToRoleAsync(company, Common.Company_Role);
+                return RedirectToPage("/Success");
+            } else {
+
+                foreach (var error in result.Errors) {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                Console.WriteLine("SignUp fail");
+
+                return Page();
+            }
+        }
+
+        public class CompanyINFO {
             [Required]
             [Display(Name = "Name")]
             public string Name { get; set; }=string.Empty;
 
             [Required]
+            [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }=string.Empty;
+
+            [Required]
+            [Phone]
+            [Display(Name ="PhoneNumber")]
+            public string PhoneNumber {get; set;}=string.Empty;
 
             [Required]
             [Display(Name = "City")]
@@ -54,48 +98,6 @@ namespace webserver.Pages.Manage {
             [DataType(DataType.Password)]
             [Display(Name = "ConfirmPassword")]
             public string ConfirmPassword { get; set; }=string.Empty;
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            /*
-            if (ModelState.IsValid){
-                return;
-            }
-            */            
-
-            var auxUser = await _userManager.FindByEmailAsync(Input.Email);
-            if (_userManager == null) {
-                ModelState.AddModelError(string.Empty, "manager not registered!");
-                return Page();
-            }
-            if(auxUser != null){
-                ModelState.AddModelError(string.Empty, "Email already registered!");
-                return Page();
-            }
-
-            var company = new Company { UserName = Input.Name, Email = Input.Email, Country = Input.Country, State = Input.State, City = Input.City };
-
-            var result = await _userManager.CreateAsync(company, Input.Password);
-
-            if (result.Succeeded) {
-
-                var roleExist = await _roleManager.RoleExistsAsync(Common.Company_Role);
-                if (!roleExist){
-                    await _roleManager.CreateAsync(new IdentityRole(Common.Company_Role));
-                }
-
-                await _userManager.AddToRoleAsync(company, Common.Company_Role);
-                return RedirectToPage("/Success");
-            } else {
-
-                foreach (var error in result.Errors) {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-                Console.WriteLine("SignUp fail");
-
-                return Page();
-            }
         }
     }
 }
