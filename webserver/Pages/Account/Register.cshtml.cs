6cc4180290f190e436aff7beed747f3a853a97c1
Matheus Lacerda <math2.0@hotmail.com>
2023-09-01 21:40:44 -0300

    bzemployee register fixed

--- webserver/Pages/Account/Register.cshtml.cs
+++ webserver/Pages/Account/Register.cshtml.cs
@@ -12,16 +12,12 @@ namespace webserver.Pages.Account {
         [BindProperty]
         public RegisterInputModel RegisterInput { get; set; } = new RegisterInputModel();
 
-        private readonly UserManager<Company> _userManager;
-        private readonly SignInManager<Company> _signInManager;
+        private readonly UserManager<BZEmployee> _userManager;
+        private readonly SignInManager<BZEmployee> _signInManager;
 
-        public RegisterModel(UserManager<Company> userManager, SignInManager<Company> signInManager) {
+        public RegisterModel(UserManager<BZEmployee> userManager, SignInManager<BZEmployee> signInManager) {
             _userManager = userManager;
             _signInManager = signInManager;
-        }
-
-        public void OnGet() {
-            // Initialization or any other logic when the page is loaded
         }
 
         //public IActionResult OnPost() {
@@ -31,21 +27,17 @@ namespace webserver.Pages.Account {
                 return Page();
             }*/
 
-            // Create a new user based on the registration input
             var user = new BZEmployee {
                 UserName = RegisterInput.Name,
                 Email = RegisterInput.Email,
-                PhoneNumber=RegisterInput.PhoneNumber,
-                City = RegisterInput.City,
-                State = RegisterInput.State,
-                Country = RegisterInput.Country
+                PhoneNumber=RegisterInput.PhoneNumber
             };
 
-            var result = await _userManager.CreateAsync(company, RegisterInput.Password);
+            var result = await _userManager.CreateAsync(user, RegisterInput.Password);
 
             if (result.Succeeded) {
                 // Optionally, you can sign in the user after successful registration
-                await _signInManager.SignInAsync(company, isPersistent: false);
+                await _signInManager.SignInAsync(user, isPersistent: false);
 
                 // Redirect to a success page or the desired destination
                 return RedirectToPage("/Privacy");
@@ -57,7 +49,6 @@ namespace webserver.Pages.Account {
                 return Page();
             }
         }
-
 
         public class RegisterInputModel {
 
