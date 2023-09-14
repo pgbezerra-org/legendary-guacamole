077d4279ab7b0737a8b0056e9da139cdff7d6cc4
Matheus Lacerda <math2.0@hotmail.com>
2023-09-14 01:54:45 -0300

    RealEstate class new fields added

--- webserver/Models/RealEstate.cs
+++ webserver/Models/RealEstate.cs
@@ -7,8 +7,22 @@ public class RealEstate {
     public string Address { get; set; } = string.Empty;
     public decimal Price { get; set; }
 
+    public bool rentable = false;
+
+    public int area = 16;
+
+    public int percentage = 100;
+
+    public int numBedrooms = 1;
+
+    public HouseType houseType;
+
     // Foreign key to associate the RealEstate with a Company
     public string CompanyId { get; set; }=string.Empty;
     // Navigation property
     public Company? OwnerCompany { get; set; }
+}
+
+public enum HouseType {
+    house = 1, condominium, apartment
 }
