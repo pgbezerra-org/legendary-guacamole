8039f91d9d3ac862bce748e5e1e107ba23f77c12
Matheus Lacerda <math2.0@hotmail.com>
2023-09-27 21:29:07 -0300

    RealEstateDTO

---
+++ webserver/Models/DTOs/RealEstateDTO.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webserver.Models;
+
+namespace webserver.Models.DTOs {
+    public class RealEstateDTO {
+        
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public string CompanyId { get; set; } = string.Empty;
+
+        public RealEstateDTO(int id, string name, string address, decimal price, string companyId){
+
+            Id = id;
+            Name = name;
+            Address = address;
+            Price = price;
+            CompanyId = companyId;
+        }
+
+        public static explicit operator RealEstate(RealEstateDTO DTO) {
+            return new RealEstate {
+                Id = DTO.Id,
+                Name = DTO.Name,
+                Address = DTO.Address,
+                Price = DTO.Price,
+                CompanyId = DTO.CompanyId
+            };
+        }
+
+        public static explicit operator RealEstateDTO(RealEstate RE){
+            return new RealEstateDTO (RE.Id, RE.Name, RE.Address, RE.Price, RE.CompanyId);
+        }
+    }
+}
