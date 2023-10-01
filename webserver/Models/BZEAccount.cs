039acba1bdd83021d0d22467753b990c02e795ee
Matheus Lacerda <math2.0@hotmail.com>
2023-10-01 19:15:28 -0300

    namespaces identation

--- webserver/Models/BZEAccount.cs
+++ webserver/Models/BZEAccount.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace webserver.Models
-{
-    public class BZEAccount : IdentityUser
-    {
-        public DateTime LastLogin { get; set; }
-    }
+namespace webserver.Models;
+
+public class BZEAccount : IdentityUser {
+    public DateTime LastLogin { get; set; }
 }
--- webserver/Models/Common.cs
+++ webserver/Models/Common.cs
@@ -1,17 +1,17 @@
-namespace webserver.Models {
-    public static class Common {
+namespace webserver.Models;
 
-        public const string BZE_Cookie = "BZE_Auth";
-        public const string Company_Cookie = "Company_Auth";
-        public const string Client_Cookie = "Client_Auth";
-        
-        public const string BZE_Role = "BZE_Role";
-        public const string Company_Role = "Company_Role";
-        public const string Client_Role = "Client_Role";
+public static class Common {
 
-        public enum HouseType {
-            house = 1, condominium, apartment
-        }
-        
-    }    
-}
+    public const string BZE_Cookie = "BZE_Auth";
+    public const string Company_Cookie = "Company_Auth";
+    public const string Client_Cookie = "Client_Auth";
+    
+    public const string BZE_Role = "BZE_Role";
+    public const string Company_Role = "Company_Role";
+    public const string Client_Role = "Client_Role";
+
+    public enum HouseType {
+        house = 1, condominium, apartment
+    }
+    
+}    
