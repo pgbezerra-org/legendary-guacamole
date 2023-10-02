3e992b610e7523d4e6aa4715fe8a08568ab9470c
Matheus Lacerda <math2.0@hotmail.com>
2023-10-02 18:09:14 -0300

    most tests done

--- webserver.Tests/Project/Controllers/RealEstatesControllerTest.cs
+++ webserver.Tests/Project/Controllers/RealEstatesControllerTest.cs
@@ -51,48 +51,60 @@ public class RealEstatesControllerTest : IDisposable {
 
         // Assert
         var response = Assert.IsType<OkObjectResult>(result);
-        string json = response.Value.ToJson();
+        string json = response.Value!.ToString()!;
+        RealEstateDTO responseRealEstate = JsonConvert.DeserializeObject<RealEstateDTO>(json)!;
 
+        Assert.Equal(responseRealEstate.Id, newRealEstate.Id);
+        Assert.Equal(responseRealEstate.Name, newRealEstate.Name);
+        Assert.Equal(responseRealEstate.Address, newRealEstate.Address);
+        Assert.Equal(responseRealEstate.Price, newRealEstate.Price);
+        Assert.Equal(responseRealEstate.CompanyId, newRealEstate.CompanyId);
     }
 
     [Fact]
     public async Task CreateRealEstate_ReturnsBadRequest_WhenRealEstateAlreadyExists() {
         // Arrange
         CompanyDTO companyDto = new CompanyDTO("q2w3e4r5", "company", "company123@hotmail.com", "5557890123", "Brazil", "RJ", "RJ");
+        var newRealEstate = new RealEstateDTO(2, "rio state", "copacabana", 11, companyDto.Id);
 
         _context.Company.Add((Company)companyDto);
+        _context.RealEstates.Add((RealEstate)newRealEstate);
         _context.SaveChanges();
 
-        var newRealEstate = new RealEstateDTO(2, "rio state", "copacabana", 11, companyDto.Id);
-        _context.RealEstates.Add((RealEstate)newRealEstate);
-
+        // Act
         var result = await _controller.CreateRealEstate(newRealEstate);
 
+        // Arrange
         var response = Assert.IsType<BadRequestObjectResult>(result);
         Assert.Equal(response.Value, "Real Estate Already Exists!");
     }
 
     [Fact]
     public async Task CreateRealEstate_ReturnsBadRequest_WhenOwnerCompanyNotExists() {
-        // Assert
-    
-        // Act
-    
         // Arrange
+        int stateId = 11;
+        var newRealEstate = new RealEstateDTO(stateId, "rio state", "copacabana", 11000, "noCompanyHere");
+
+        // Act
+        var result = await _controller.CreateRealEstate(newRealEstate);
+
+        // Arrange
+        var response = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal(response.Value, "Owner Company does Not Exist!");
     }
 
-    [Fact]
+    [Fact]//BUGADA
     public async Task ReadRealEstates_ReturnsOkResult_WithValidParameters() {
 
-        Company comp = new Company{UserName="initialcomp",Email="initialcomp@gmail.com"};
+        Company comp = new Company{Id="c0mp4=ny55", UserName="initialcomp",Email="initialcomp@gmail.com"};
         _context.Company.Add(comp);
 
-        _context.RealEstates.Add(new RealEstate { Id = 2, Address="Hollywood Boulevard", Name = "Property2", Price = 200});
-        _context.RealEstates.Add(new RealEstate { Id = 3, Address="Sunset Boulevard", Name = "Property3", Price = 99});
-        _context.RealEstates.Add(new RealEstate { Id = 4, Address="The Bar", Name = "Property4", Price = 100});
-        _context.RealEstates.Add(new RealEstate { Id = 5, Address="Something", Name = "Property5", Price = 101});
-        _context.RealEstates.Add(new RealEstate { Id = 6, Address="Anything", Name = "Property6", Price = 102});
-        _context.RealEstates.Add(new RealEstate { Id = 7, Address="Whatsoever", Name = "Property7", Price = 103});
+        _context.RealEstates.Add(new RealEstate { Id = 2, Address="Hollywood Boulevard", Name = "Property2", Price = 200, CompanyId=comp.Id});
+        _context.RealEstates.Add(new RealEstate { Id = 3, Address="Sunset Boulevard", Name = "Property3", Price = 99, CompanyId=comp.Id});
+        _context.RealEstates.Add(new RealEstate { Id = 4, Address="The Bar", Name = "Property4", Price = 100, CompanyId=comp.Id});
+        _context.RealEstates.Add(new RealEstate { Id = 5, Address="Something", Name = "Property5", Price = 101, CompanyId=comp.Id});
+        _context.RealEstates.Add(new RealEstate { Id = 6, Address="Anything", Name = "Property6", Price = 102, CompanyId=comp.Id});
+        _context.RealEstates.Add(new RealEstate { Id = 7, Address="Whatsoever", Name = "Property7", Price = 103, CompanyId=comp.Id});
         _context.SaveChanges();
         
         // Act
@@ -126,11 +138,12 @@ public class RealEstatesControllerTest : IDisposable {
 
     [Fact]
     public async Task ReadRealEstate_ReturnsOkResult_WhenRealEstateExists() {
-
         // Arrange
         int idToRead = 76;
+        Company comp = new Company { Id="a1b1c1d1", UserName = "exampleCompany"};
         RealEstateDTO realDto = new RealEstateDTO(idToRead, "Sesame Street", "Sesame House", 40, "a1b1c1d1");
 
+        _context.Company.Add(comp);
         _context.RealEstates.Add((RealEstate)realDto);
         _context.SaveChanges();
 
@@ -140,13 +153,13 @@ public class RealEstatesControllerTest : IDisposable {
         // Assert
         var response = Assert.IsType<OkObjectResult>(result);
         string valueJson = response.Value!.ToString()!;
-        RealEstateDTO[] realEstatesDtoArray = JsonConvert.DeserializeObject<RealEstateDTO[]>(valueJson)!;
+        RealEstateDTO realEstateDto = JsonConvert.DeserializeObject<RealEstateDTO>(valueJson)!;
 
-        Assert.Equal(realDto.Id, realEstatesDtoArray[0].Id);
-        Assert.Equal(realDto.Address, realEstatesDtoArray[0].Address);
-        Assert.Equal(realDto.Name, realEstatesDtoArray[0].Name);
-        Assert.Equal(realDto.Price, realEstatesDtoArray[0].Price);
-        Assert.Equal(realDto.CompanyId, realEstatesDtoArray[0].CompanyId);
+        Assert.Equal(realDto.Id, realEstateDto.Id);
+        Assert.Equal(realDto.Address, realEstateDto.Address);
+        Assert.Equal(realDto.Name, realEstateDto.Name);
+        Assert.Equal(realDto.Price, realEstateDto.Price);
+        Assert.Equal(realDto.CompanyId, realEstateDto.CompanyId);
     }
 
     [Fact]
@@ -161,8 +174,15 @@ public class RealEstatesControllerTest : IDisposable {
     [Fact]
     public async Task UpdateRealEstate_ReturnsOkResult_WhenRealEstateExists() {
         // Assert
+        CompanyDTO companyDto = new CompanyDTO("q2w3e4r5", "company", "company123@hotmail.com", "5557890123", "Brazil", "RJ", "RJ");
+        RealEstateDTO newRealEstate = new RealEstateDTO(2, "rio state", "copacabana", 11, companyDto.Id);
+
+        _context.Company.Add((Company)companyDto);
+        _context.RealEstates.Add((RealEstate)newRealEstate);
+        _context.SaveChanges();
     
         // Act
+
     
         // Arrange
     }
@@ -181,7 +201,9 @@ public class RealEstatesControllerTest : IDisposable {
         // Arrange
         int idExist = 76;
         RealEstateDTO realDto = new RealEstateDTO(idExist, "Sesame Street", "Sesame House", 40, "a1b1c1d1");
+        CompanyDTO companyDto = new CompanyDTO("a1b1c1d1", "company", "company123@hotmail.com", "5557890123", "Brazil", "RJ", "RJ");
 
+        _context.Company.Add((Company)companyDto);
         _context.RealEstates.Add((RealEstate)realDto);
         _context.SaveChanges();
 
