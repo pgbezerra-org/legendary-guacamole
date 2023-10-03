5ca83984870f5f4541d4cecd2ef7e08cda78c23f
Matheus Lacerda <math2.0@hotmail.com>
2023-10-03 01:35:43 -0300

    Profile Pages of RealEstates and Companies

---
+++ webserver/Pages/Views/RealEstateProfile.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using webserver.Data;
+using webserver.Models;
+
+namespace webserver.Pages.Views;
+[Authorize]    
+public class RealEstateProfile : PageModel {
+    public WebserverContext context;
+
+    public RealEstate? realEstate;
+    public Company? OwnerCompany;
+
+    public RealEstateProfile(WebserverContext _context) {
+        context = _context;
+    }
+
+    public void OnGet(int id) {
+
+        realEstate = context.RealEstates.Find(id);
+
+        if(realEstate==null){
+            Console.WriteLine("ERROR 404");
+            return;
+        }
+
+        OwnerCompany = context.Company.Find(realEstate.CompanyId);
+    }
+}
