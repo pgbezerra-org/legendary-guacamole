6ec5f1f87f9344de388ea9490d79c6da32793713
Matthew Lacerda <math2.0@hotmail.com>
2023-10-03 01:31:38 -0300

    New Manage Pages (#30)
    * New Manage Pages
    
    * some whitespacing

--- webserver/Pages/Manage/Overview.cshtml.cs
+++ webserver/Pages/Manage/Overview.cshtml.cs
@@ -1,26 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+namespace webserver.Pages.Manage;
+[Authorize]
+public class Overview : PageModel {
 
-namespace webserver.Pages.Manage
-{
-    [Authorize]
-    public class Overview : PageModel
-    {
-        private readonly ILogger<Overview> _logger;
+    public Overview() {
+    }
 
-        public Overview(ILogger<Overview> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet() {
     }
 }
