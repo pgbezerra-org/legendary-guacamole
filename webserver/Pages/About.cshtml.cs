a6637b25e9cef3c0c4023e5a652d592289074976
Matheus Lacerda <math2.0@hotmail.com>
2023-10-03 01:47:15 -0300

    Improved the Starting point of the website's Front-End

--- webserver/Pages/About.cshtml.cs
+++ webserver/Pages/About.cshtml.cs
@@ -1,24 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace webserver.Pages
-{
-    public class About : PageModel
-    {
-        private readonly ILogger<About> _logger;
+namespace webserver.Pages;
+public class About : PageModel {
 
-        public About(ILogger<About> logger)
-        {
-            _logger = logger;
-        }
+    public About() {
+        
+    }
 
-        public void OnGet()
-        {
-        }
+    public void OnGet() {
     }
 }
